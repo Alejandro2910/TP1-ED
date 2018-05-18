4ec4e6c11c13476f1b86daa60d549076d649da40
Alejandro2910 <32439030+Alejandro2910@users.noreply.github.com>
2018-05-17 19:11:49 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include "ListaPosicionadaLSE.h"
+#include "ListaPosicionadaLDE.h"
+#include "ListaIndexadaLSE.h"
+#include "ListaOrd_LSE.h"
+#include "ListaOrd_Array.h"
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
