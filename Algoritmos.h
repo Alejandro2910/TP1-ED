4fa74da6ca0d59e5ce4999cfe5df473400cd3db8
Andrés Mesén <Andrs Mesn>
2018-05-23 01:26:01 -0600

    Algoritmos: Listar, Simetrica, Eliminar, DosListasIguales

--- Algoritmos.h
+++ Algoritmos.h
@@ -22,11 +22,90 @@ class Algoritmos
                     Lista.Intercambiar(iter, iter2);
                     iter=iter->ptrNext;
                     iter2=Lista.Anterior(iter2);
-                    if((iter->ptrSig)==iter2){
+                    if((iter->ptrNext)==iter2){
                         casoFinal=true;
                     }
                 }
             }
+        }
+
+        void Listar(ListaPosicionadaLSE Lista)
+        {
+            CajaS<A> *ptr=Lista.Primera();
+            while(ptr!=nullptr)
+            {
+                std::cout<<ptr->elemento<<" ";
+                ptr=ptr->ptrNext;
+            }
+            //std::cout<<std::endl;
+        }
+
+        bool Simetrica(ListaPosicionadaLSE Lista){
+            CajaS<A> *iter=Lista.Primera();
+            CajaS<A> *iter2=Lista.Ultima();
+            if((Lista.NumElem()%2)!=0){
+                while(iter!=iter2){
+                    if(iter->elemento != iter2->elemento)
+                        return false;
+                    iter=iter->ptrNext;
+                    iter2=Lista.Anterior(iter2);
+                }
+            }else{
+                bool casoFinal=false;
+                while(!casoFinal){
+                    if(iter->elemento != iter2->elemento)
+                        return false;
+                    iter=iter->ptrNext;
+                    iter2=Lista.Anterior(iter2);
+                    if((iter->ptrNext)==iter2){
+                        casoFinal=true;
+                    }
+                }
+            }
+            return true;
+        }
+
+        void EliminarRepetidos(ListaPosicionadaLSE Lista)
+        {
+            CajaS<A> *ptr1=Lista.Primera();
+            CajaS<A> *ptr2;
+            while(ptr1->ptrNext!=nullptr)
+            {
+                ptr2=ptr1->ptrNext;
+                while(ptr2!=nullptr)
+                {
+                    if(ptr1->elemento == ptr2->elemento)
+                        Lista.Borrar(ptr2);
+                    ptr2=ptr2->ptrNext;
+                }
+                ptr1=ptr1->ptrNext;
+            }
+        }
+
+        bool DosListasIguales(ListaPosicionadaLSE L1, ListaPosicionadaLSE L2)
+        {
+            if(L1.NumElem()!= L2.NumElem())
+                return false;
+
+            CajaS<A> *ptr1=L1.Primera();
+            CajaS<A> *ptr2;
+            bool coincidencia;
+            while(ptr1!=nullptr)
+            {
+                coincidencia=false;
+                ptr2=L2.Primera();
+                while(ptr2!=nullptr && !coincidencia)
+                {
+                    if(ptr1->elemento == ptr2->elemento)
+                        coincidencia=true;
+                    ptr2=ptr2->ptrNext;
+                }
+                if(!coincidencia)
+                    return false;
+                ptr1=ptr1->ptrNext;
+            }
+            return true;
+
         }
         Algoritmos(){}
         virtual ~Algoritmos(){}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include "Algoritmos.h"
 #include "ListaPosicionadaLSE.h"
 #include "ListaPosicionadaLDE.h"
 #include "ListaIndexadaLSE.h"
@@ -11,5 +12,49 @@ using namespace std;
 
 int main()
 {
+    ListaPosicionadaLSE l;
+    l.Iniciar();
+    l.Insertar(5,l.Primera());
+    l.Insertar(8, l.Siguiente(l.Primera()));
+    l.AgregarAlFinal(9);
+    l.AgregarAlFinal(23);
+    l.AgregarAlFinal(15);
+
+    ListaPosicionadaLSE lp;
+    lp.Iniciar();
+    lp.Insertar(1, lp.Primera());
+    lp.AgregarAlFinal(1);
+    lp.AgregarAlFinal(9);
+    lp.AgregarAlFinal(1);
+    lp.AgregarAlFinal(8);
+
+    ListaPosicionadaLSE lp2;
+    lp2.Iniciar();
+    lp2.Insertar(1, lp2.Primera());
+    lp2.AgregarAlFinal(1);
+    lp2.AgregarAlFinal(9);
+    lp2.AgregarAlFinal(1);
+    lp2.AgregarAlFinal(5);
+
+    cout<<"L= "; l.imprimir();
+    Algoritmos<int> a;
+    a.invertir(l);
+    cout<<endl<<"\nL= "; a.Listar(l);
+    cout<<endl<<"\nLp= "; a.Listar(lp);
+
+    if(a.Simetrica(lp))
+        cout<<"\nEs simetrica";
+    else
+        cout<<"\nNo simetrica";
+
+    //a.EliminarRepetidos(lp);
+    //cout<<endl<<"\nLp= "; a.Listar(lp);
+
+    cout<<endl<<"\nLp= "; a.Listar(lp);
+    cout<<endl<<"\nLp2= "; a.Listar(lp2);
+    if(a.DosListasIguales(lp, lp2))
+        cout<<"\nSon iguales";
+    else
+        cout<<"\nNo son iguales";
     return 0;
 }
